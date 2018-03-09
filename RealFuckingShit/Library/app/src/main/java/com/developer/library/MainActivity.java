4853295c67cfcdc955e85b121e0322016c1964e3
ANONYMOUS <iamcicada3301@gmail.com>
2018-03-09 17:56:27 +0530

    added shared preference protocol

--- RealFuckingShit/Library/app/src/main/java/com/developer/library/MainActivity.java
+++ RealFuckingShit/Library/app/src/main/java/com/developer/library/MainActivity.java
@@ -25,15 +25,16 @@ public class MainActivity extends AppCompatActivity {
     FirebaseAuth mAuth;
     List<AuthUI.IdpConfig> providers;
     Button signOut;
-    SharedPreferences isLoggedIn;
+    SharedPreferences preferences;
+    SharedPreferences.Editor editor;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        signOut = findViewById(R.id.sign_out);
-        mAuth = FirebaseAuth.getInstance();
+        preferences = this.getSharedPreferences("isLoggedIn", MODE_PRIVATE);
+        editor = preferences.edit();
 
         providers = Arrays.asList(
                 new AuthUI.IdpConfig.EmailBuilder().build(),
@@ -41,7 +42,17 @@ public class MainActivity extends AppCompatActivity {
                 new AuthUI.IdpConfig.PhoneBuilder().build()
         );
 
-        startActivityForResult(AuthUI.getInstance().createSignInIntentBuilder().setAvailableProviders(providers).build(), 1);
+//        editor.putBoolean("loggedIn", false);
+//        editor.commit();
+        if(preferences.getBoolean("loggedIn", false)){
+            // user already logged in
+            // Toast.makeText(this, "already logged in as " + FirebaseAuth.getInstance().getCurrentUser().getDisplayName(), Toast.LENGTH_SHORT).show();
+        } else {
+            startActivityForResult(AuthUI.getInstance().createSignInIntentBuilder().setAvailableProviders(providers).build(), 1);
+        }
+
+        signOut = findViewById(R.id.sign_out);
+        mAuth = FirebaseAuth.getInstance();
 
         signOut.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -52,6 +63,8 @@ public class MainActivity extends AppCompatActivity {
                             public void onComplete(@NonNull Task<Void> task) {
                                 // user is now signed out
                                 // startActivity(new Intent(MyActivity.this, SignInActivity.class));
+                                editor.putBoolean("loggedIn", false);
+                                editor.commit();
                                 finish();
                             }
                         });
@@ -69,7 +82,10 @@ public class MainActivity extends AppCompatActivity {
             if (resultCode == RESULT_OK) {
                 // Successfully signed in
                 FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
-                Toast.makeText(this, "" + user.getDisplayName().split(" ").toString().toLowerCase(), Toast.LENGTH_SHORT).show();
+                // Toast.makeText(this, "" + user.getDisplayName().split(" ").toString().toLowerCase(), Toast.LENGTH_SHORT).show();
+                // Toast.makeText(this, "" + user.getDisplayName(), Toast.LENGTH_SHORT).show();
+                editor.putBoolean("loggedIn", true);
+                editor.commit();
                 // ...
             } else {
                 // Sign in failed, check response for error code
